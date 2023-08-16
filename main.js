854a38c1d20477e745185705971e746506f4a2c9
Baraa N. Mohsin <87875062+BaraaNazar@users.noreply.github.com>
2023-08-16 19:56:41 +0300

    Add keyboard support

--- main.js
+++ main.js
@@ -9,100 +9,127 @@ const clearButton = document.querySelector('.clearBtn');
 const undoButton = document.querySelector('.minusPlusBtn');
 const remainderButton = document.querySelector('.remainderBtn');
 
-let currentCalculation = "";
+let currentCalculation = '';
 let currentResult = 0;
-let sign = "";
+let sign = '';
 let newOperation = true;
 
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const buttonValue = button.innerHTML;
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const buttonValue = button.innerHTML;
-
-        if (newOperation) {
-            currentCalculation = buttonValue;
-            newOperation = false;
-        } else {
-            currentCalculation += buttonValue;
-        }
-        display.innerHTML = currentCalculation;
-    });
+    if (newOperation) {
+      currentCalculation = buttonValue;
+      newOperation = false;
+    } else {
+      currentCalculation += buttonValue;
+    }
+    display.innerHTML = currentCalculation;
+  });
 });
 
 plusButton.addEventListener('click', () => {
-    operate();
-    sign = "+";
-    newOperation = true;
+  operate();
+  sign = '+';
+  newOperation = true;
 });
 
 minusButton.addEventListener('click', () => {
-    operate();
-    sign = "-";
-    newOperation = true;
+  operate();
+  sign = '-';
+  newOperation = true;
 });
 
 multiplyButton.addEventListener('click', () => {
-    operate();
-    sign = "*";
-    newOperation = true;
+  operate();
+  sign = '*';
+  newOperation = true;
 });
 
 dividerButton.addEventListener('click', () => {
-    operate();
-    sign = "/";
-    newOperation = true;
+  operate();
+  sign = '/';
+  newOperation = true;
 });
 
 remainderButton.addEventListener('click', () => {
-    operate();
-    sign = "%";
-    newOperation = true;
+  operate();
+  sign = '%';
+  newOperation = true;
 });
 
 equalsButton.addEventListener('click', () => {
-    operate();
-    sign = "";
-    newOperation = true;
+  operate();
+  sign = '';
+  newOperation = true;
 });
 
 clearButton.addEventListener('click', () => {
-    currentCalculation = "";
-    currentResult = 0;
-    sign = "";
-    newOperation = true;
-    display.innerHTML = "0";
+  currentCalculation = '';
+  currentResult = 0;
+  sign = '';
+  newOperation = true;
+  display.innerHTML = '0';
 });
 
 undoButton.addEventListener('click', () => {
-    if (currentCalculation.length > 0) {
-        currentCalculation = currentCalculation.slice(0, -1);
-        display.innerHTML = currentCalculation;
-    }
+  if (currentCalculation.length > 0) {
+    currentCalculation = currentCalculation.slice(0, -1);
+    display.innerHTML = currentCalculation;
+  }
 });
 
 function operate() {
-    if (currentCalculation !== "") {
-        const currentValue = parseFloat(currentCalculation);
-        if (sign === "+") {
-            currentResult += currentValue;
-        } else if (sign === "-") {
-            currentResult -= currentValue;
-        } else if (sign === "*") {
-            currentResult *= currentValue;
-        } else if (sign === "%") {
-            currentResult = (currentValue / 100) * currentResult;
-            currentResult = parseFloat(currentResult.toFixed(2));
-        }else if (sign === "/") {
-            if (currentValue === 0) {
-                display.innerHTML = "error";
-                return;
-            }
-            currentResult /= currentValue;
-            currentResult = currentResult.toFixed(2);
-        } else {
-            currentResult = currentValue;
-        }
-        display.innerHTML = currentResult;
-        currentCalculation = "";
+  if (currentCalculation !== '') {
+    const currentValue = parseFloat(currentCalculation);
+    if (sign === '+') {
+      currentResult += currentValue;
+    } else if (sign === '-') {
+      currentResult -= currentValue;
+    } else if (sign === '*') {
+      currentResult *= currentValue;
+    } else if (sign === '%') {
+      currentResult = (currentValue / 100) * currentResult;
+      currentResult = parseFloat(currentResult.toFixed(2));
+    } else if (sign === '/') {
+      if (currentValue === 0) {
+        display.innerHTML = 'error';
+        return;
+      }
+      currentResult /= currentValue;
+      currentResult = currentResult.toFixed(2);
+    } else {
+      currentResult = currentValue;
     }
+    display.innerHTML = currentResult;
+    currentCalculation = '';
+  }
 }
+// Add keyboard link to the calculator
+document.addEventListener('keydown', (event) => {
+  const key = event.key;
+
+  // Check if the pressed key is a number or operator key
+  if (
+    !isNaN(key) ||
+    key === '+' ||
+    key === '-' ||
+    key === '*' ||
+    key === '/' ||
+    key === '%' ||
+    key === '=' ||
+    key === 'Enter'
+  ) {
+    // Simulate a click on the corresponding button
+    const button = document.querySelector(`.keyboard[data-key="${key}"]`);
+    if (button) {
+      button.click();
+    }
+  } else if (key === 'Backspace') {
+    // Simulate a click on the undo button
+    undoButton.click();
+  } else if (key === 'Escape') {
+    // Simulate a click on the clear button
+    clearButton.click();
+  }
+});
